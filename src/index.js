6587bc43629f1d16a753ee4c331f9fff750ede53
MariaNanys <maria.iwaszczenko@gmail.com>
2025-02-10 16:09:50 +0100

    Create counter on numbers elements

--- src/index.js
+++ src/index.js
@@ -34,4 +34,33 @@ $(document).ready(function (){
              $(navMenu).animate({top:'-330px'}, 1000)
         }  
     }); 
-});
+        $(window).on('scroll', function () {
+            $('.intro_count').each(function () {
+                if (isVisible($(this))) {
+                    startCounter($(this));
+                    $(this).removeClass('intro_count');
+                }
+            });
+        });
+});  
+function isVisible(element) {
+    let win = $(window);
+    let viewportTop = win.scrollTop();
+    let viewportBottom = viewportTop + win.height();
+    let elementTop = element.offset().top;
+    let elementBottom = elementTop + element.height();
+        
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+}
+    
+function startCounter(element) {
+    element.prop('Counter', 0).animate({
+        Counter: element.text()
+    }, {
+        duration: 3000,
+        easing: 'swing',
+        step: function (now) {
+            element.text(Math.ceil(now));
+        }
+    });
+}
