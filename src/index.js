35423030ed71f9619e567c531da1084452863b9c
MariaNanys <maria.iwaszczenko@gmail.com>
2025-02-10 13:43:14 +0100

    Create scrollTo and add jquery

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles.scss';
+import $ from './jquery';
 
 let barBtn = document.querySelector(".nav-bar");
 let barLine1 = document.querySelector(".bar1");
@@ -12,5 +13,13 @@ function toggleBar() {
     barLine3.classList.toggle("change");
 
 }
+
+$(document).ready(function (){
+    $("a").click(function (a){
+        $('html, body').animate({
+            scrollTop: $(a.target.hash).offset().top
+        }, 2000);
+    });
+});
 
 barBtn.addEventListener("click", toggleBar);
